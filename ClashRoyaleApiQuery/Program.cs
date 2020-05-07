0acf86c083f003ddfebf71bc4f1269c220b4974e
Michael Schuetze <mjschuetze102@gmail.com>
2020-05-07 02:27:02 -0400

    Successfully connected to API

--- ClashRoyaleApiQuery/Program.cs
+++ ClashRoyaleApiQuery/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ClashRoyaleApiQuery
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
+
+        static async Task<string> ConnectToAPI(string apiKey, string clanTag)
+        {
+            client.BaseAddress = new Uri("https://api.clashroyale.com/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
+
+            HttpResponseMessage response = await client.GetAsync($"clans/%23{clanTag}/warlog");
+
+            string message = await response.Content.ReadAsStringAsync();
+            return message;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            Console.WriteLine(ConnectToAPI(config["clash_royale_api_key"], config["clash_royale_clan_tag"]).GetAwaiter().GetResult());
         }
     }
 }
