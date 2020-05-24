e8f0d21170c2f94e17d47971c2570fd5de663235
Michael Schuetze <mjschuetze102@gmail.com>
2020-05-24 01:10:23 -0400

    Application now properly gathers information and stores it in a database

--- ClashRoyaleApiQuery/DataStorage.cs
+++ ClashRoyaleApiQuery/DataStorage.cs
@@ -1,4 +1,5 @@
-﻿using ClashRoyaleDataModel.Models;
+﻿using ClashRoyaleDataModel.DatabaseContexts;
+using ClashRoyaleDataModel.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,16 @@ namespace ClashRoyaleApiQuery
     class DataStorage
     {
         DataCollection data;
-        HashSet<Player> players = new HashSet<Player>();
+        ClanParticipationContext context;
+        HashSet<Player> players;
+        HashSet<WarLog> previousWars;
 
-        public DataStorage(DataCollection data)
+        public DataStorage(DataCollection data, ClanParticipationContext context)
         {
             this.data = data;
+            this.context = context;
+            players = context.ClanMembers.ToHashSet();
+            previousWars = context.WarHistory.ToHashSet();
         }
 
         public void StoreData()
@@ -20,25 +26,38 @@ namespace ClashRoyaleApiQuery
             IEnumerable<WarLog> warLogs = data.GetWarLogs();
             IEnumerable<DonationRecord> donationRecords = data.GetDonationRecords();
 
+            HashSet<WarLog> newWars = new HashSet<WarLog>();
+
             foreach (WarLog warlog in warLogs)
             {
+                if (previousWars.Contains(warlog))
+                    continue;
+
                 foreach (WarParticipation participation in warlog.Participants)
                 {
                     Player player = GetPlayerFromDatabase(participation.Player);
                     player.WarParticipations.Add(participation);
+                    participation.Player = player;
                 }
+
+                newWars.Add(warlog);
             }
 
             foreach (DonationRecord record in donationRecords)
             {
                 Player player = GetPlayerFromDatabase(record.Player);
                 player.DonationRecords.Add(record);
+                record.Player = player;
             }
 
             foreach (Player player in players)
             {
                 Console.WriteLine($"{player.Name,15}, {(player.DonationRecords.Count > 0 ? player.DonationRecords.First().Donations : 0),3}, {player.WarParticipations.Count,2}");
             }
+
+            context.WarHistory.AddRange(newWars);
+            context.DonationRecords.AddRange(donationRecords);
+            context.SaveChanges();
         }
 
         private Player GetPlayerFromDatabase(Player player)
--- ClashRoyaleApiQuery/Program.cs
+++ ClashRoyaleApiQuery/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ClashRoyaleDataModel.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ClashRoyaleApiQuery
 {
@@ -6,14 +10,36 @@ namespace ClashRoyaleApiQuery
     {
         static void Main(string[] args)
         {
+            CreateHostBuilder(args).Build();
+
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<ClanParticipationContext>(options => {
+                    options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                    options.EnableSensitiveDataLogging(true);
+                })
+                .BuildServiceProvider();
+
+            var context = serviceProvider.GetRequiredService<ClanParticipationContext>();
+
             ApiConnection api = new ApiConnection(config["clash_royale_api_url"], config["clash_royale_api_key"]);
             DataCollection data = new DataCollection(api, config["clash_royale_clan_tag"]);
-            DataStorage storage = new DataStorage(data);
+            DataStorage storage = new DataStorage(data, context);
             storage.StoreData();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    IConfiguration config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json", true, true)
+                        .Build();
+
+                    services.AddDbContext<ClanParticipationContext>(options => options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+                });
     }
 }
