49a77c764a15d4b681519b2c5fdd4ff82f36da10
Michael Schuetze <mjschuetze102@gmail.com>
2020-06-06 00:26:13 -0400

    Add new API endpoint

---
+++ ClashRoyale/Controllers/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using ClashRoyaleDataModel.DatabaseContexts;
+using ClashRoyaleDataModel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClashRoyale.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ClanParticipationContext _context;
+
+        public PlayerController(ClanParticipationContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// GET: /api/player
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<Player>> GetPlayers()
+        {
+            var players = await _context.ClanMembers
+                .Include(c => c.WarParticipations)
+                .Include(c => c.DonationRecords)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return players;
+        }
+    }
+}
